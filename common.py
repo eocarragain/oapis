f88a6b45a4ba38ee3027a6dc975437f34e2512f7
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2018-01-15 22:34:35 +0000

    remove unused #requests_retry_session helper method

--- common.py
+++ common.py
@@ -40,27 +40,6 @@ class Common(object):
         doi = doi.strip()
         return doi
 
-    # Helper method for robust http retries
-    # See https://www.peterbe.com/plog/best-practice-with-retries-with-requests
-    def requests_retry_session(
-        retries=3,
-        backoff_factor=0.5,
-        status_forcelist=(500, 502, 503, 504),
-        session=None,
-    ):
-        session = session or requests.Session()
-        retry = Retry(
-            total=retries,
-            read=retries,
-            connect=retries,
-            backoff_factor=backoff_factor,
-            status_forcelist=status_forcelist,
-        )
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        return session
-
     def create_cache_dir(self):
         os.makedirs(self.cache_dir, exist_ok=True)
 
